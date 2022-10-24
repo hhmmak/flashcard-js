4f71ed8621c2a41ab98765957cb8a7e891ba7beb
Hermanda Mak <hermandamak@gmail.com>
2022-10-24 15:35:23 -0700

    add bootstrap container component

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import CardAdd from './components/CardAdd';
 import CardEdit from './components/CardEdit';
 import CardDetail from './components/CardDetail';
 
+import Container from 'react-bootstrap/Container';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Main />} />
-        <Route path="/new" element={<CardAdd />} />
-        <Route path="/edit" element={<CardEdit />} />
-        <Route path="/:id" element={<CardDetail/>} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/new" element={<CardAdd />} />
+          <Route path="/edit" element={<CardEdit />} />
+          <Route path="/:id" element={<CardDetail/>} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
