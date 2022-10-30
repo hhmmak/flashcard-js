cbc0b6c4719bd2a69778e341ba8ee23d050b7ea5
Hermanda Mak <hermandamak@gmail.com>
2022-10-30 15:59:09 -0700

    add useEffect and render contents

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 import Button from "react-bootstrap/esm/Button";
 
+
 const CardDetail = () => {
+
+  const [cards, setCards] = useState([]);
+
+  useEffect( () => {
+    axios.get(`http://localhost:5000/api/flashcard`)
+      .then(res => setCards(res.data))
+      .catch(err => console.log(err));
+  
+  }, []);
+
+  const flipCard = () => {
+    //flip flashcard function
+  }
+
+
   return (
     <div>
-      <p>Type</p>
-      <p>Title</p>
-      <Button>Flip</Button>
-      <p>Description</p>
+    { cards.map( (card, index) =>
+      <div key={index}>
+        <p>{card.type}</p>
+        <p>{card.word}</p>
+        <Button>Flip</Button>
+        <p>
+          {card.description.split("\n").map( (sentence, index) => 
+            <div key={index}>{sentence}</div>
+          )}
+        </p>
+      </div>
+    )}
     </div>
   )
 }
