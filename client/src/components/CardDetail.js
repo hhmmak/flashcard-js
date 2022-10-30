53cc4b4cbb3acf16a634e3a4fbe5e219fb1346c4
Hermanda Mak <hermandamak@gmail.com>
2022-10-30 16:01:27 -0700

    minor change

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-import Button from "react-bootstrap/esm/Button";
+import Button from "react-bootstrap/Button";
 
 
 const CardDetail = () => {
