f2caa0e9a56780b4c67075a0a64adda41d4efaaa
Hermanda Mak <hermandamak@gmail.com>
2022-10-30 14:45:18 -0700

    set up card template

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -1,6 +1,12 @@
+import Button from "react-bootstrap/esm/Button";
+
 const CardDetail = () => {
   return (
-    <div>CardDetail</div>
+    <div>
+      <p>Title</p>
+      <Button>Flip</Button>
+      <p>Description</p>
+    </div>
   )
 }
 export default CardDetail;
