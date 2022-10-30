b83ef76da36ab53dfab46500e3146ed70274f758
Hermanda Mak <hermandamak@gmail.com>
2022-10-30 15:58:35 -0700

    comment out incomplete components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './views/Main';
-import CardAdd from './components/CardAdd';
-import CardEdit from './components/CardEdit';
+// import CardAdd from './components/CardAdd';
+// import CardEdit from './components/CardEdit';
 import CardDetail from './components/CardDetail';
 
 import Container from 'react-bootstrap/Container';
@@ -13,9 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Main />} />
-          <Route path="/new" element={<CardAdd />} />
-          <Route path="/edit" element={<CardEdit />} />
-          <Route path="/:id" element={<CardDetail/>} />
+          {/* <Route path="/new" element={<CardAdd />} />
+          <Route path="/edit" element={<CardEdit />} /> */}
+          <Route path="/card" element={<CardDetail/>} />
         </Routes>
       </BrowserRouter>
     </Container>
