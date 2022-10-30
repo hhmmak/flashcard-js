b257409dbd9e97efc4bdda0ba892821f3c87ea49
Hermanda Mak <hermandamak@gmail.com>
2022-10-30 14:58:24 -0700

    add type

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/esm/Button";
 const CardDetail = () => {
   return (
     <div>
+      <p>Type</p>
       <p>Title</p>
       <Button>Flip</Button>
       <p>Description</p>
