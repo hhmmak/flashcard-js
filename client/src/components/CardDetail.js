9b6bff5ce458e66c6fc47df7c9b8e21f31e87e8e
Hermanda Mak <hermandamak@gmail.com>
2022-11-12 19:38:51 -0800

    add psuedo effect for flipping flashcard

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
 const CardDetail = ({card}) => {
 
-  const flipCard = () => {
-    //flip flashcard function
+  const [flip, setFlip] = useState(false);
+
+  const flipCard = e => {
+    setFlip(!flip);
   }
 
   return (
     <div>
         <p>{card.type}</p>
         <p>{card.word}</p>
-        <Button>Flip</Button>
+        <Button onClick={flipCard} variant={flip? "danger" : "primary"}>Flip</Button>
         <div>
           {card.description.split("\n").map( (sentence, index) => 
             <div key={index}>{sentence}</div>
