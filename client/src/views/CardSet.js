38d9e78ef1c42f8fd071d9049fa0192f5812fd19
Hermanda Mak <hermandamak@gmail.com>
2022-11-12 19:33:54 -0800

    initial commit

---
+++ client/src/views/CardSet.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CardDetail from "../components/CardDetail";
+
+const CardSet = () => {
+
+  const [cards, setCards] = useState([]);
+
+  useEffect( () => {
+    axios.get(`http://localhost:5000/api/flashcard`)
+      .then(res => setCards(res.data))
+      .catch(err => console.log(err));
+  
+  }, []);
+
+  return (
+    <div>
+    { cards.map( (card, index) =>
+      <CardDetail key={index} card={card}/>
+    )}
+    </div>
+  )
+}
+export default CardSet;
