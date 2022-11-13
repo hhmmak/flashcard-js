91734c9e81451a9a4cf1ac5669ce84d2710d437a
Hermanda Mak <hermandamak@gmail.com>
2022-11-12 19:34:23 -0800

    refactor component with CardSet

--- client/src/components/CardDetail.js
+++ client/src/components/CardDetail.js
@@ -1,39 +1,21 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-
 import Button from "react-bootstrap/Button";
 
-
-const CardDetail = () => {
-
-  const [cards, setCards] = useState([]);
-
-  useEffect( () => {
-    axios.get(`http://localhost:5000/api/flashcard`)
-      .then(res => setCards(res.data))
-      .catch(err => console.log(err));
-  
-  }, []);
+const CardDetail = ({card}) => {
 
   const flipCard = () => {
     //flip flashcard function
   }
 
-
   return (
     <div>
-    { cards.map( (card, index) =>
-      <div key={index}>
         <p>{card.type}</p>
         <p>{card.word}</p>
         <Button>Flip</Button>
-        <p>
+        <div>
           {card.description.split("\n").map( (sentence, index) => 
             <div key={index}>{sentence}</div>
           )}
-        </p>
-      </div>
-    )}
+        </div>
     </div>
   )
 }
