7e272dca6225a82019db0f705402c45f3686cf32
Hermanda Mak <hermandamak@gmail.com>
2022-11-19 19:10:37 -0800

    add props

--- client/src/components/CardForm.js
+++ client/src/components/CardForm.js
@@ -1,21 +1,26 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const CardForm = (props) => {
+const CardForm = ({card, setCard, submitHandler}) => {
+
+  const onChangeHandler = (e) => {
+    //TODO setCard
+  }
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3">
           <Form.Label>Type</Form.Label>
-          <Form.Control type="text" name='type'/>
+          <Form.Control type="text" name='type' onChange={onChangeHandler} value={card.type}/>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Word</Form.Label>
-          <Form.Control type="text" name='word'/>
+          <Form.Control type="text" name='word' onChange={onChangeHandler} value={card.word}/>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Description</Form.Label>
-          <Form.Control type="text" name='description'/>
+          <Form.Control type="text" name='description' onChange={onChangeHandler} value={card.description}/>
         </Form.Group>
         <Button type="submit">Submit</Button>
       </Form>
