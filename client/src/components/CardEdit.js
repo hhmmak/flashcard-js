9a7d8c65deb378e183325b139f0c88e1d25ca854
Hermanda Mak <hermandamak@gmail.com>
2022-11-21 23:09:26 -0800

    add fetch api for flashcard detail and edit

--- client/src/components/CardEdit.js
+++ client/src/components/CardEdit.js
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import CardForm from "./CardForm"
 
-const CardEdit = () => {
+const CardEdit = ({id}) => {
+
+  const[loaded, setLoaded] = useState(false);
   const [card, setCard] = useState({
     "type": "",
     "word": "",
     "description": ""
   });
 
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/flashcard/${id}`)
+    .then( res => {
+      setCard(res.data);
+      setLoaded(true);
+    })
+    .catch(err => console.log(err))
+  }, [id])
+
   const editSubmit = () => {
-    //TODO submit fetch
+    axios.put(`http://localhost:8000/api/flashcard/${id}`)
+      .then( res => {
+        console.log(res);
+        //TODO what to do after edit
+      })
+      .catch(err => console.log(err));
   }
 
   return (
-    <div>
-      <CardForm card={card} setCard={setCard} submitHandler={editSubmit}/>
-    </div>
+  <>
+    { loaded &&
+      <div>
+        <CardForm card={card} setCard={setCard} submitHandler={editSubmit}/>
+      </div>
+    }
+  </>
   )
 }
 export default CardEdit
