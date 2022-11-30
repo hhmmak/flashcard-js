5ca7e270bd08c63f753ba78ae37f919238544be4
Hermanda Mak <hermandamak@gmail.com>
2022-11-30 13:22:09 -0800

    add form routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Main from './views/Main';
-// import CardAdd from './components/CardAdd';
-// import CardEdit from './components/CardEdit';
-import CardSet from './views/CardSet';
+import CardAdd from './components/CardAdd';
+import CardEdit from './components/CardEdit';
+import CardSet from './features/CardSet';
 
 import Container from 'react-bootstrap/Container';
 
@@ -12,10 +11,9 @@ function App() {
     <Container>
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Main />} />
-          {/* <Route path="/new" element={<CardAdd />} />
-          <Route path="/edit" element={<CardEdit />} /> */}
-          <Route path="/card" element={<CardSet />} />
+          <Route path="/" element={<CardSet />} />
+          <Route path="/new" element={<CardAdd />} />
+          <Route path="/edit" element={<CardEdit />} />
         </Routes>
       </BrowserRouter>
     </Container>
