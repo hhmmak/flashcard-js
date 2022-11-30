ebcf961eedb1f32798885883572f76d0144a2ef3
Hermanda Mak <hermandamak@gmail.com>
2022-11-30 13:22:30 -0800

    create add function and form display

---
+++ client/src/components/CardAdd.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
+import CardForm from "./CardForm"
+import { useNavigate } from "react-router-dom";
+
+const CardAdd = () => {
+
+  const navigate = useNavigate();
+
+  const [card, setCard] = useState({
+    "type": "",
+    "word": "",
+    "description": ""
+  });
+
+  const editSubmit = (newCard) => {
+    axios.post(`http://localhost:5000/api/flashcard`, newCard)
+      .then( res => {
+        console.log(res);
+        navigate('/card');
+      })
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div>
+      <CardForm card={card} setCard={setCard} submitHandler={editSubmit}/>
+    </div>
+  )
+}
+export default CardAdd;
