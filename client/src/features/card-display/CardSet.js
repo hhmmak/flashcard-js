50a6190297028977249cf2b325af56757f58b05e
Hermanda Mak <hermandamak@gmail.com>
2022-12-07 11:44:26 -0800

    add navigation to add new card

--- client/src/features/card-display/CardSet.js
+++ client/src/features/card-display/CardSet.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-import CardDetail from "./CardDetail";
+import CardDetail from "./CardDetail/CardDetail";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
 
 const CardSet = () => {
 
+  const nav = useNavigate();
   const [cards, setCards] = useState([]);
 
   useEffect( () => {
@@ -16,6 +19,7 @@ const CardSet = () => {
 
   return (
     <div>
+      <Button onClick={() => nav("./new")}>New Term</Button>
     { cards.map( (card, index) =>
       <CardDetail key={index} card={card}/>
     )}
