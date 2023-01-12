848244678508259c4ab2a270f58109c739812bd3
Hermanda Mak <hermandamak@gmail.com>
2023-01-11 16:46:42 -0800

    fix minor bug

--- client/src/features/card-form/CardForm.js
+++ client/src/features/card-form/CardForm.js
@@ -13,23 +13,21 @@ const CardForm = ({card, setCard, submitHandler}) => {
   }
 
   return (
-    <div>
-      <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Type</Form.Label>
-          <Form.Control type="text" name='type' onChange={onChangeHandler} value={card.type}/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Word</Form.Label>
-          <Form.Control type="text" name='word' onChange={onChangeHandler} value={card.word}/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" row={5} name='description' onChange={onChangeHandler} value={card.description}/>
-        </Form.Group>
-        <Button type="submit">Submit</Button>
-      </Form>
-    </div>
+    <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Type</Form.Label>
+        <Form.Control type="text" name='type' onChange={onChangeHandler} value={card.type}/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Word</Form.Label>
+        <Form.Control type="text" name='word' onChange={onChangeHandler} value={card.word}/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" rows={7} name='description' onChange={onChangeHandler} value={card.description}/>
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
   )
 }
 export default CardForm
