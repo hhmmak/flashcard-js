40baf6d8598a9d96cd6fb3a9843afe537a2de6e5
Hermanda Mak <hermandamak@gmail.com>
2023-01-19 16:58:07 -0800

    add edit button

--- client/src/features/card-display/CardDetail/CardDetail.js
+++ client/src/features/card-display/CardDetail/CardDetail.js
@@ -4,9 +4,11 @@ import styles from './CardDetail.module.css';
 import Button from "react-bootstrap/Button";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import { useNavigate } from "react-router-dom";
 
 const CardDetail = ({card}) => {
 
+  const navigate = useNavigate();
   const [flip, setFlip] = useState(false);
 
   const flipCard = e => {
@@ -20,6 +22,7 @@ const CardDetail = ({card}) => {
             <p>{card.type}</p>
             <h2>{card.word}</h2>
             <Button onClick={flipCard} variant={flip? "danger" : "primary"}>Flip</Button>
+            <Button onClick={() => navigate(`/edit`, {state: {id : card._id}})}>Edit</Button>
           </Col>
           <Col sm={7}>
             {flip && card.description.split("\n").map( (sentence, index) =>
