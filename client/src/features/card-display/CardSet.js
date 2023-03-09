b25da6f5baf38cf2a799b1a2d457d376bc248cf3
Hermanda Mak <hermandamak@gmail.com>
2023-03-08 23:21:17 -0800

    convert bootstrap components to tailwind

--- client/src/features/card-display/CardSet.js
+++ client/src/features/card-display/CardSet.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 import CardDetail from "./CardDetail/CardDetail";
-import { useNavigate } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Button from "react-bootstrap/Button";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 const CardSet = () => {
 
@@ -22,16 +22,16 @@ const CardSet = () => {
 
   return (
     <div>
-      <Button size={"lg"} onClick={() => nav("./new")}>New Term</Button>
-        <Container>
-          <Row>
+      <button onClick={() => nav("./new")} className={`border rounded bg-cyan-600 text-white px-5 py-2`}>New Term</button>
+        <div className="container mx-auto">
+          <div className="grid sm:grid-cols-2 gap-2">
           { cards.map( (card, index) =>
-            <Col lg={6} key={index}>
+            <div key={index}>
               <CardDetail card={card}/>
-            </Col>
+            </div>
           )}
-          </Row>
-        </Container>
+          </div>
+        </div>
     </div>
   )
 }
